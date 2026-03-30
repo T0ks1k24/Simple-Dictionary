2354cf1c6c7a6387ee919b867a71c5df6cab3cef
T0ks1k24 <toksamal2004@gmail.com>
2026-03-30 15:34:42 +0300

    feat: migrate database from SQLite to PostgreSQL and add documentation

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':     os.environ.get('POSTGRES_DB',       'lexicon'),
+        'USER':     os.environ.get('POSTGRES_USER',     'lexicon'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'lexicon'),
+        'HOST':     os.environ.get('POSTGRES_HOST',     'db'),
+        'PORT':     os.environ.get('POSTGRES_PORT',     '5432'),
     }
 }
 
